2525f73a5211d02d97d05af0ddc9457828c3b493
carlos-garciamoran <46545682+carlos-garciamoran@users.noreply.github.com>
2023-02-05 20:50:03 -0800

    Initial Commit

---
+++ TweetWidget/TweetWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TweetWidgetApp.swift
+//  TweetWidget
+//
+//  Created by Carlos García Morán on 2/5/23.
+//
+
+import SwiftUI
+
+@main
+struct TweetWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
